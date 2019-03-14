e1403090b76b12887faba09902a5be1a8bf654be
Pierce Kotarski <pierejkotarski@utexas.edu>
2019-03-14 16:38:14 -0500

    Creation of interworking/dynamic Flask web applicaiton. Layout still needs updating and styling.

--- main.py
+++ main.py
@@ -3,29 +3,46 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index(): #this should route to the landing page
     return render_template('index.html')
 
 @app.route('/sharks')
 def sharks():
     return render_template('sharks.html')
 
-
-@app.route('/companies')
-def companies():
-    return render_template('companies.html')
-
-
-@app.route('/companies/<compName>')
-def thisCompany(compName):
-    return render_template('company.html', compName)
+@app.route('/deals')
+def deals():
+    return render_template('deals.html')
 
 
-@app.route('/<shark>')
+@app.route('/shark/<shark>')
 def thisShark(shark):
-    return render_template("shark.html", shark)
+    return render_template('thisShark.html')
+
+@app.route('/company/<thisComp>')
+def thisComp(thisComp):
+    return render_template('thisComp.html')
+
+'''
+@app.route('/oleary')
+def oleary(shark):
+    return render_template("kevin_oleary.html")
+
+@app.route('/cuban') #why don't we do it by
+def cuban(shark):
+    return render_template("mark_cuban.html")
+
+@app.route('/daymond_john')
+def john():#
+    return render_template('john.html')
 
 
+@app.route('/herjavec')
+def herjavec(compName):
+    return render_template('robert_herjavec.html')
+
+'''
 
 if __name__ == "__main__":
+
     app.run(debug = True)
---
+++ static/js/deals.js
@@ -0,0 +1,435 @@
+
+var json = [
+   {
+       "sharks": [{
+           "name" : "Kevin O'Leary",
+           "invested" : 8543000,
+           "deals" : 40,
+           "episodes" : 131
+         }],
+       "company": {
+         "name" : "Zipz",
+         "episode" : "6-11",
+         "founders" : ["Andrew McMurray"],
+         "location" : "Santa Rosa, CA",
+         "description" : "Single-serve wines in recyclable, high quality plastic stemware that looks and feels like glass.",
+         "investment" : 2500000,
+         "equity" : 0.10
+       }
+   },
+
+   {
+       "sharks": [{
+           "name" : "Kevin O'Leary",
+           "invested" : 8543000,
+           "deals" : 40,
+           "episodes" : 131
+         },
+         {
+           "name" : "Mark Cuban",
+           "invested" : 19850000,
+           "deals" : 85,
+           "episodes" : 111
+         }],
+       "company": {
+         "name" : "Toygaroo",
+         "episode" : "2-2",
+         "founders" : ["Nikki Pope"],
+         "location" : "Los Angeles, CA",
+         "description" : "A subscription toy service.",
+         "investment" : 200000,
+         "equity" : 0.40
+       }
+   },
+
+   {
+       "sharks": [{
+           "name" : "Kevin O'Leary",
+           "invested" : 8543000,
+           "deals" : 40,
+           "episodes" : 131
+         }],
+       "company": {
+         "name" : "How Do You Roll?",
+         "episode" : "4-16",
+         "founders" : ["Peter Yung", "Yuen Yung"],
+         "location" : "Austin, TX",
+         "description" : "A chain of quick-service establishments selling quality made-to-order sushi rolls.",
+         "investment" :  1000000,
+         "equity" : 0.20
+       }
+   },
+
+   {
+       "sharks": [{
+           "name" : "Kevin O'Leary",
+           "invested" : 8543000,
+           "deals" : 40,
+           "episodes" : 131
+         },
+         {
+           "name" : "Robert Herjavec",
+           "invested" : 16598333,
+           "deals" : 57,
+           "episodes" : 126
+         }],
+       "company": {
+         "name" : "Jump Forward",
+         "episode" : "6-11",
+         "founders" : ["Brian Duggan", "Adam McCombs"],
+         "location" : "Chicago, IL",
+         "description" : "  An online service allowing high school athletes and their parents to profile their skills to market themselves to colleges.",
+         "investment" : 150000,
+         "equity" : 0.10
+       }
+   },
+
+   {
+       "sharks": [{
+           "name" : "Robert Herjavec",
+           "invested" : 16598333,
+           "deals" : 57,
+           "episodes" : 126
+         }],
+       "company": {
+         "name" : "Zero Pollution Motors",
+         "episode" : "6-27",
+         "founders" : ["Ethan Tucker", "Pat Boone"],
+         "location" : "New Paltz, NY",
+         "description" : "Zero Pollution Motors is launching the urban car of the future, the AIRPod. The AIRPod vehicle is the solution to urban pollution and urban mobility. With its small size, a tiny price, zero pollution, and a fun and futuristic design, AIRPod marks a turning point in the range of urban vehicles. It is a real breath of fresh air in cities and the prelude to travel without pollution.",
+         "investment" : 5000000,
+         "equity" : 0.50
+       }
+   },
+
+   {
+       "sharks": [{
+           "name" : "Robert Herjavec",
+           "invested" : 16598333,
+           "deals" : 57,
+           "episodes" : 126
+         }],
+       "company": {
+         "name" : "Chord Buddy",
+         "episode" : "3-3",
+         "founders" : ["Travis Perry"],
+         "location" : "New Paltz, NY",
+         "description" : "A two-month learning system that teaches which chords to play on the guitar.",
+         "investment" : 175000,
+         "equity" : 0.20
+       }
+   },
+
+   {
+       "sharks": [{
+           "name" : "Robert Herjavec",
+           "invested" : 16598333,
+           "deals" : 57,
+           "episodes" : 126
+         },
+         {
+           "name" : "Jeff Foxworthy",
+           "invested" : 75000,
+           "deals" : 2,
+           "episodes" : 2
+         },
+         {
+           "name" : "Daymond John",
+           "invested" : 8567000,
+           "deals" : 61,
+           "episodes" : 100
+         }],
+       "company": {
+         "name" : "Hillbilly Brand",
+         "episode" : "2-4",
+         "founders" : ["Mike Abbaticchio", "Shon Lees"],
+         "location" : "Parkland, FL",
+         "description" : "A country-style apparel store sold through Sports Authority, truck stops and travel plazas.",
+         "investment" : 75000,
+         "equity" : 1.00
+       }
+   },
+
+   {
+       "sharks": [{
+           "name" : "Robert Herjavec",
+           "invested" : 16598333,
+           "deals" : 57,
+           "episodes" : 126
+         }],
+       "company": {
+         "name" : "Happy Feet",
+         "episode" : "5-23",
+         "founders" : ["Pat Yates"],
+         "location" : "    Louisville, KY",
+         "description" : "Happy Feet are the plushest, softest slippers that money can buy. These slippers don't apologize for being comfy, they flaunt it. Founded in 1995, the company started out with just a few styles but over the years have expanded to include hundreds of designs, including many in association with branding partners.",
+         "investment" : 375000,
+         "equity" : 0.25
+       }
+   },
+
+   {
+      "sharks": [{
+          "name" : "Mark Cuban",
+          "invested" : 19850000,
+          "deals" : 85,
+          "episodes" : 111
+        }],
+      "company": {
+        "name": "BeatBox Beverages",
+        "episode" : "6-6",
+        "founders" : ["Brad Schultz", "Aimy Steadman", "Justin Fenchel"],
+        "location" : "Austin, TX",
+        "description" : "Inexpensive and creatively-flavored box wine",
+        "investment" : 1000000,
+        "equity" : 0.33
+      }
+    },
+
+    {
+      "sharks": [{
+          "name" : "Mark Cuban",
+          "invested" : 19850000,
+          "deals" : 85,
+          "episodes" : 111
+        }],
+      "company": {
+        "name": "Gameday Couture",
+        "episode" : "6-11",
+        "founders" : ["Shawna Feddersen", "Kurt Feddersen"],
+        "location" : "Tulsa, OK",
+        "description" : "Fashionable and affordably-priced apparel for female sports fans",
+        "investment" : 500000,
+        "equity" : 0.30
+      }
+    },
+    {
+      "sharks": [{
+          "name" : "Mark Cuban",
+          "invested" : 19850000,
+          "deals" : 85,
+          "episodes" : 111 },
+        {
+          "name" : "Barbara Corcoran",
+          "invested" : 5465000,
+          "deals" : 53,
+          "episodes" : 82
+        }],
+
+      "company": {
+        "name": "LOLIWARE",
+        "episode" : "7-2",
+        "founders" : ["Leigh Ann Tucker","Chelsea Briganti"],
+        "location" : "New York, NY",
+        "description" : "LOLIWARE are the first compostable, biodegradable cups that are also edible and delicious! These two college friends created these cups as an eco-friendly and fun alternative to party supplies. Made with vegetable gelatin, LOLIWARE is completely vegan",
+        "investment" : 600000,
+        "equity" : 0.25
+      }
+    },
+
+   {
+      "sharks": [{
+          "name" : "Mark Cuban",
+          "invested" : 19850000,
+          "deals" : 85,
+          "episodes" : 111
+        },
+        {
+          "name" : "Barbara Corcoran",
+          "invested": 5465000,
+          "deals" : 53,
+          "episodes" : 82
+        }],
+      "company": {
+        "name": "Villy Customs",
+        "episode" : "3-13",
+        "founders" : ["Fleetwood Hicks"],
+        "location" : "Dallas, TX",
+        "description" : "An online store for customizable bicycles",
+        "investment" : 500000,
+        "equity" : 0.42
+    }
+  },
+    {
+      "sharks": [{
+          "name" : "Daymond John",
+          "invested" : 8567000,
+          "deals" : 61,
+          "episodes" : 100
+        }],
+      "company": {
+        "name" : "TITIN",
+        "episode" : "6-7",
+        "founders" : ["Patrick Whaley"],
+        "location" : "Atlanta, GA",
+        "description" : "TITIN is patented form-fitting weighted compression gear using gel weights as inserts.",
+        "investment" : 500000,
+        "equity" : 0.20
+      }
+    },
+    {
+      "sharks": [{
+          "name" : "Daymond John",
+          "invested" : 8567000,
+          "deals" : 61,
+          "episodes" : 100
+        }],
+      "company": {
+        "name" : "One Sole",
+        "episode" : "2-9",
+        "founders" : ["Dominique McClain Barteet"],
+        "location" : "Palm City, FL",
+        "description" : "Shoes with interchangeable uppers allowing the user to easily vary their look.",
+        "investment" : 500000,
+        "equity" : 0.35
+      }
+    },
+    {
+      "sharks": [{
+          "name" : "Daymond John",
+          "invested" : 8567000,
+          "deals" : 61,
+          "episodes" : 100
+        }],
+      "company": {
+        "name" : "Hells Bells Helmet",
+        "episode" : "1-11",
+        "founders" : ["Marix Stone", "Dr. Nancy Tanchel"],
+        "location" : "Philadelphia, PA",
+        "description" : "Designer helmets. The company owns a utility patent to place 3D designs on motorcycle helmets as well as any other kind of sporting helmet.",
+        "investment" : 500000,
+        "equity" : 0.50
+      }
+    },
+    {
+      "sharks": [{
+          "name" : "Daymond John",
+          "invested" : 8567000,
+          "deals" : 61,
+          "episodes" : 100
+        }],
+
+      "company": {
+        "name" : "Spikeball",
+        "episode" : "6-29",
+        "founders" : ["Chris Ruder"],
+        "location" : "Chicago, IL",
+        "description" : "Spikeball started out as a casual outdoors game, but has grown to become a national sensation. Played in a 2 vs. 2 format, teams compete to spike a ball into a bouncy net until one team can't return it, which awards their opposing team a point. This nascent sport is often described as a cross between beach volleyball and foursquare. Spikeball's popularity has been growing, and currently there are over a quarter million players in the United States, as well as over a thousand nationally ranked teams.",
+        "investment" : 500000,
+        "equity" : 0.22
+      }
+  }
+]
+
+
+function sharks(){
+
+  var deals = json;
+  //this is an array of deals {}, each one with two values 1 of which is company info, next is shark info
+  var allSharks = [];
+  var allSharkInfo = [];
+  for (var i = 0; i < deals.length; i++){
+    var involvedSharks = deals[i]["sharks"];
+    for (var j = 0; j<involvedSharks.length; j++){
+      if (!allSharks.includes((involvedSharks[j])["name"])){
+        allSharks.push((involvedSharks[j])["name"]); //push the entire value of this shark
+        allSharkInfo.push(involvedSharks[j]);
+      }
+    }
+  }
+
+  for (var l in allSharks){
+    //allSharks is your info on the name of the given shark your at
+    url = toURL(allSharks[l]);
+    document.getElementById('list').innerHTML += '<a href = shark/' + url + ' class = "list-group-item list-group-item-action active">'+ allSharks[l] + '</a>';
+  }
+
+  console.log(allSharks[2].localeCompare(allSharks[3]));
+}
+
+function deals(){
+  var deals = json;
+  for (i=0; i<deals.length; i++){
+    var compName = (deals[i]["company"])["name"];
+    var compLink = compName.replace(/ /g, "_");
+    document.getElementById('list').innerHTML += '<a href = company/' + compLink + ' class = "list-group-item list-group-item-action active">'+ compName + '</a>';
+  }
+}
+
+function thisShark(){
+  //name should be of first_last and you will need to remove any '
+  var hyper = window.location.href;
+  var possibles = hyper.split('/');
+  var name = possibles[possibles.length-1];
+  var deals = json;
+  var theseDeals = [];
+  var dealsInfo = [];
+  for (var i = 0; i < deals.length; i++){
+    var theseSharks = deals[i]["sharks"];
+    for (var k = 0; k < theseSharks.length; k++){
+      var thisName = (theseSharks[k]["name"]).toString();
+      //document.getElementById('list').innerHTML += '<a href = "#" class = "list-group-item list-group-item-action active">'+ thisName + '</a>';
+
+      thisName = thisName.replace(/'/g, "");
+      thisName = thisName.replace(/ /g, "_");
+
+
+      if (thisName.toUpperCase() == name.toUpperCase()){
+        theseDeals.push(deals[i]["company"]["name"]);
+        dealsInfo.push(deals[i]["company"]);
+      }
+    }
+  }
+
+  for (var j = 0; j<theseDeals.length; j++){
+    var compName = theseDeals[j];
+    var compLink = compName.replace(/ /g, "_");
+    document.getElementById('list').innerHTML += '<a href = ../company/' + compLink + ' class = "list-group-item list-group-item-action active">'+ compName + '</a>';
+  }
+}
+
+function thisComp(){
+
+  var hyper = window.location.href;
+  var possibles = hyper.split('/');
+  var name = possibles[possibles.length-1];
+  var deals = json;
+
+
+
+  for (var i = 0; i < deals.length; i++){
+    var thisName = toURL(deals[i]["company"]["name"]);
+
+
+    if (thisName.toUpperCase().localeCompare(name.toUpperCase()) == 0){
+
+      console.log(name);
+      console.log(thisName);
+      break;
+    }
+  }
+
+  var involvedSharks = deals[i]["sharks"];
+
+  console.log(involvedSharks);
+
+  for (var j = 0; j< involvedSharks.length; j++){
+
+    var url = toURL(involvedSharks[j]["name"]);
+    document.getElementById('list').innerHTML += '<a href = ../shark/' + url + ' class = "list-group-item list-group-item-action active">'+ involvedSharks[j]["name"] + '</a>';
+  }
+}
+
+function apos(str){
+  return str.replace(/'/g, "");
+}
+
+function toURL(str){
+    str = str.toLowerCase();
+    str = str.replace(/'/g, "");
+    return str.replace(/ /g, "_");
+}
+
+function fromURL(str){
+  return str.replace(/_/g, " ");
+}
